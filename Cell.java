5b3225a6b4048645f439050d09ce806a1fadfa8f
Ethan F <eflemingservices@gmail.com>
2024-04-12 18:56:03 -0400

    Updated variables for init test

--- Cell.java
+++ Cell.java
@@ -1,20 +1,33 @@
 public class Cell {
     private String oem;
     private String model;
-    private Integer launchAnnounced;
+    private String launchAnnounced;
     private String launchStatus;
     private String bodyDimensions;
-    private Float bodyWeight;
+    private String bodyWeight;
     private String bodySim;
     private String displayType;
-    private Float displaySize;
+    private String displaySize;
     private String displayResolution;
     private String featuresSensors;
     private String platformOs;
 
     public Cell(String oem, String model, String launchAnnounced, String launchStatus,
-                String bodyDimensions, String bodyWeight, String bodySim, String displayType,
-                String displaySize, String displayResolution, String featuresSensors, String platformOs) {
-        
+            String bodyDimensions, String bodyWeight, String bodySim, String displayType,
+            String displaySize, String displayResolution, String featuresSensors, String platformOs) {
+        this.oem = oem;
+        this.model = model;
+        this.launchAnnounced = launchAnnounced;
+        this.launchStatus = launchStatus;
+        this.bodyDimensions = bodyDimensions;
+        this.bodyWeight = bodyWeight;
+        this.bodySim = bodySim;
+        this.displayType = displayType;
+        this.displaySize = displaySize;
+        this.displayResolution = displayResolution;
+        this.featuresSensors = featuresSensors;
+        this.platformOs = platformOs;
     }
+
+    
 }
