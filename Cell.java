d9a73607e46c16aaef5b60f128da54bfaecbb081
Ethan F <eflemingservices@gmail.com>
2024-04-12 17:56:18 -0400

    Base Cell created

---
+++ Cell.java
@@ -0,0 +1,20 @@
+public class Cell {
+    private String oem;
+    private String model;
+    private Integer launchAnnounced;
+    private String launchStatus;
+    private String bodyDimensions;
+    private Float bodyWeight;
+    private String bodySim;
+    private String displayType;
+    private Float displaySize;
+    private String displayResolution;
+    private String featuresSensors;
+    private String platformOs;
+
+    public Cell(String oem, String model, String launchAnnounced, String launchStatus,
+                String bodyDimensions, String bodyWeight, String bodySim, String displayType,
+                String displaySize, String displayResolution, String featuresSensors, String platformOs) {
+        
+    }
+}
